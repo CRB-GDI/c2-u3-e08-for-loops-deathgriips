a8b6784a36bd42d9b84adcd40c70223fa6020571
adam ^.^ <145371242+deathgriips@users.noreply.github.com>
2024-02-28 13:15:16 -0500

    Completed Problem 1
    Step I left off on: Step 2, Problem 2

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 1
+
+            Console.Write("Enter a number (0-100): ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Create the for loop
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+
+            // Problem 2
+
+            Console.Write("Enter a row width: ");
+
+        }
+    }
+}
